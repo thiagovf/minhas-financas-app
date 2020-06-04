e3f6f7f165c1d30306295d46156a24b62452bc15
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-06-04 17:23:23 -0300

    adicionando coisas novas ;)

---
+++ src/main/rotas.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Route, Switch, HashRouter } from 'react-router-dom'
+
+import Login from '../views/login'
+import CadastroUsuario from '../views/cadastroUsuario'
+
+function Rotas() {
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/cadastroUsuario" component={CadastroUsuario} />
+            </Switch>
+        </HashRouter>
+    )
+}
+
+export default Rotas
---
+++ src/views/cadastroUsuario.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import Card from '../components/card'
+import FormGroup from '../components/form-group'
+
+class CadastroUsuario extends React.Component {
+
+    state = {
+        nome: '',
+        email: '',
+        senha: '',
+        senhaRepeticao: ''
+    }
+
+    cadastrar = () => {
+        console.log(this.state)
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <Card title='Cadastro de Usuário'>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+                                <FormGroup label="Nome: *" htmlFor="inputNome">
+                                    <input type="text" id="inputNome" name="nome" className="form-control"
+                                        onChange={e => this.setState({nome: e.target.value})}/> 
+                                </FormGroup>
+                                <FormGroup label="Email: *" htmlFor="inputEmail">
+                                    <input type="text" id="inputEmail" name="email" className="form-control"
+                                        onChange={e => this.setState({email: e.target.value})} />
+                                </FormGroup>
+                                <FormGroup label="Senha: *" htmlFor="inputSenha">
+                                    <input type="password" id="inputSenha" name="senha" className="form-control"
+                                        onChange={e => this.setState({senha: e.target.value})} />
+                                </FormGroup>
+                                <FormGroup label="Repita a senha: *" htmlFor="inputRepitaSenha">
+                                    <input type="password" id="inputRepitaSenha" name="repitaSenha" className="form-control"
+                                        onChange={e => this.setState({senhaRepeticao: e.target.value})} />
+                                </FormGroup>
+                                <button type="button" className="btn btn-success" onClick={this.cadastrar}>Salvar</button>
+                                <button type="button" className="btn btn-danger">Cancelar</button>
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default CadastroUsuario
