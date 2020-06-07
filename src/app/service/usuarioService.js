a3c9ca9e02d8bfed9778ebb08210544d736aec5b
thiagovf <4077420+thiagovf@users.noreply.github.com>
2020-06-06 21:01:39 -0300

    adicionando serviços no frontend

---
+++ src/app/service/usuarioService.js
@@ -0,0 +1,10 @@
+import ApiService from '../apiservice'
+
+class UsuarioService extends ApiService {
+
+    constructor() {
+        super('/api/usuarios')
+    }
+}
+
+export default UsuarioService
